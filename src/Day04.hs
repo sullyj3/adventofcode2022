31fa9611eaba7fa37c2833fdf61fa04f3791e938
James Sully <sullyj3@gmail.com>
2022-12-04 17:04:58 +1100

    Switch day04 to use new solution style

--- src/Day04.hs
+++ src/Day04.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE RecordWildCards #-}
 module Day04 where
 
 import Utils (tReadMaybe, count)
@@ -6,6 +7,8 @@ import qualified Data.Text as T
 import qualified Data.Text.IO as T
 import Data.Text (Text)
 import Data.Maybe (fromJust)
+
+import AOC
 
 type ElfPair = (Range, Range)
 type Range = (Int, Int)
@@ -18,20 +21,11 @@ parseRange = twoListToPair . fromJust . traverse tReadMaybe . T.splitOn "-"
 parseElfPair :: Text -> ElfPair
 parseElfPair = twoListToPair . map parseRange . T.splitOn ","
 
-parse :: Text -> [ElfPair]
-parse = map parseElfPair . T.lines
-
 oneContainsOther :: Range -> Range -> Bool
 oneContainsOther a b = (a `rangeContains` b) || (b `rangeContains` a)
 
 rangeContains :: Range -> Range -> Bool
 rangeContains (a, b) (c, d) = a <= c && b >= d
-
-solvePart1 :: [ElfPair] -> Int
-solvePart1 = count $ uncurry oneContainsOther
-
-solvePart2 :: [ElfPair] -> Int
-solvePart2 = count $ uncurry overlaps
 
 -- | ... |
 --    | ... |
@@ -48,13 +42,17 @@ solvePart2 = count $ uncurry overlaps
 overlaps :: Range -> Range -> Bool
 overlaps (a,b) (c, d) = not $ (a<c && b<c) || (a>c && a>d)
 
-testData = [
-  ((1,2), (2,4)),
-  ((5,6), (6,7)),
-  ((1,2), (6,7))
-  ]
 
-main = do
-  pairs <- parse <$> T.readFile "inputs/day04.txt"
-  putStrLn $ "part 1: " <> show (solvePart1 pairs)
-  putStrLn $ "part 2: " <> show (solvePart2 pairs)
+solution = Solution {..}
+  where
+    parse :: Text -> [ElfPair]
+    parse = map parseElfPair . T.lines
+
+    solvePart1 :: [ElfPair] -> Int
+    solvePart1 = count $ uncurry oneContainsOther
+
+    solvePart2 :: [ElfPair] -> Int
+    solvePart2 = count $ uncurry overlaps
+
+    
+main = aocMain "inputs/day04.txt" solution
