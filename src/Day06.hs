8dbb0275d545682111e21baf24eb98171ce32cdb
James Sully <sullyj3@gmail.com>
2022-12-06 16:29:39 +1100

    Day 6 tweak

--- src/Day06.hs
+++ src/Day06.hs
@@ -5,22 +5,12 @@ import           Data.List     (findIndex)
 import           Relude.Unsafe (fromJust)
 import           Utils         (allDistinct, slidingWindow)
 
--------------
--- Parsing --
--------------
-parseInput ∷ Text → String
-parseInput = toString
-
----------------
--- Solutions --
----------------
-part1 ∷ String → Int
-part1 = (4+) . fromJust . findIndex allDistinct . slidingWindow 4
-
-part2 ∷ String → Int
-part2 = (14+) . fromJust . findIndex allDistinct . slidingWindow 14
+findCode ∷ Int → String → Int
+findCode n = (n+) . fromJust . findIndex allDistinct . slidingWindow n
 
 main ∷ IO ()
 main = do
-  aocMain "inputs/06.txt" Solution { parse=parseInput, part1=part1, part2=part2 }
+  aocMain "inputs/06.txt" Solution { parse=toString
+                                   , part1=findCode 4
+                                   , part2=findCode 14 }
 
