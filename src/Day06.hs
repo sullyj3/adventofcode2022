faf2286419e8a1c563fec7216ecf13e1f7d7dc69
James Sully <sullyj3@gmail.com>
2022-12-06 16:09:20 +1100

    Day 6

--- src/Day06.hs
+++ src/Day06.hs
@@ -1,21 +1,35 @@
 module Day06 (main) where
 
 import           AOC
-import           AOC.Parse
-import           AOC.Parsers
-import qualified Data.Text   as T
-import           Utils       (tRead)
+import Data.List (nub)
 
 -------------
 -- Parsing --
 -------------
-parseInput = id
+parseInput :: Text -> String
+parseInput = toString
 
 ---------------
 -- Solutions --
 ---------------
-part1 = const ()
-part2 = const ()
+
+slidingWindow :: Int -> [a] -> [[a]]
+slidingWindow n = takeWhile ((== n) . length) . map (take n) . tails
+
+allDistinct :: Eq a => [a] -> Bool
+allDistinct xs = nub xs == xs
+
+findIndex :: (a -> Bool) -> [a] -> Int
+findIndex p = go 0
+  where
+    go _ [] = error "No elements match the predicate"
+    go i (x:xs) = if p x then i else go (i + 1) xs
+
+part1 :: String -> Int
+part1 = (4+) . findIndex allDistinct . slidingWindow 4
+
+part2 :: String -> Int
+part2 = (14+) . findIndex allDistinct . slidingWindow 14
 
 main ∷ IO ()
 main = do 
