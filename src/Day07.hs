d3ca9dc5a14dc58891ef9c7d69f3ee71dfd9a388
James Sully <sullyj3@gmail.com>
2022-12-07 23:53:41 +1100

    Day 7 cleanup

--- src/Day07.hs
+++ src/Day07.hs
@@ -3,18 +3,14 @@ module Day07 (main) where
 
 import           AOC
 import           AOC.Parse            hiding (State)
-import           Data.List            (foldl1', minimum)
+import           Data.List            (minimum)
+import qualified Data.List            as List
+import qualified Data.List.NonEmpty   as NE
 import qualified Data.Text            as T
 import           Prelude              hiding (many, some)
 import           PyF
 import           Text.Megaparsec.Char (alphaNumChar, newline, string)
-import qualified Data.List.NonEmpty as NE
-import qualified Data.List as List
 
-
-data File = File { fsize :: Int }
-          | Dir  { dircontents :: [Text] } -- list of absolute paths
-  deriving (Show)
 
 data CommandAndOutput = Cd Text | CdUp | Ls [LsEntry]
   deriving (Show, Eq, Ord)
@@ -25,18 +21,10 @@ data LsEntry = F { name :: Text, size :: Int}
 
 type Session = [CommandAndOutput]
 
-instance VisualStream Session where
-  showTokens :: Proxy Session -> NonEmpty (Token Session) -> String
-  showTokens p = List.unwords . NE.toList . fmap show
-
-instance TraversableStream Session where
-  reachOffset :: Int -> PosState s -> (Maybe String, PosState s)
-  reachOffset i s = (Nothing, s)
-
 -------------
 -- Parsing --
 -------------
-parseInput ∷ Text → FileTree2
+parseInput ∷ Text → FileTree
 parseInput input = fileTree where
   session = unsafeParse (some commandAndOutputP) input
   fileTree = unsafeParse fileTreeP session
@@ -47,50 +35,60 @@ commandAndOutputP = do
   try cdUpP <|>
     try cdP <|>
     try lsP
+  where
+    cdUpP ∷ Parser CommandAndOutput
+    cdUpP = lineOf $ CdUp <$ string "cd .."
 
-cdP ∷ Parser CommandAndOutput
-cdP = lineOf $ Cd <$> (string "cd " *> fileNameP)
+    cdP ∷ Parser CommandAndOutput
+    cdP = lineOf $ Cd <$> (string "cd " *> fileNameP)
 
-cdUpP ∷ Parser CommandAndOutput
-cdUpP = lineOf $ CdUp <$ string "cd .."
+    lsP ∷ Parser CommandAndOutput
+    lsP = Ls <$> (lineOf (string "ls") *> some lsEntryP)
 
-fileNameP ∷ Parser Text
-fileNameP = T.pack <$> some (alphaNumChar <|> single '.' <|> single '/')
+    lsEntryP ∷ Parser LsEntry
+    lsEntryP = lineOf $ try lsFileP <|> lsDirP
 
-lsP ∷ Parser CommandAndOutput
-lsP = Ls <$> (lineOf (string "ls") *> some lsEntryP)
+    lsFileP ∷ Parser LsEntry
+    lsFileP = liftA2 (flip F) decimal (single ' ' *> fileNameP)
 
-lsEntryP ∷ Parser LsEntry
-lsEntryP = lineOf $ try lsFileP <|> lsDirP
+    lsDirP ∷ Parser LsEntry
+    lsDirP = string "dir " *> (D <$> fileNameP)
 
-lsDirP ∷ Parser LsEntry
-lsDirP = string "dir " *> (D <$> fileNameP)
-
-lsFileP ∷ Parser LsEntry
-lsFileP = liftA2 (flip F) decimal (single ' ' *> fileNameP)
+    fileNameP ∷ Parser Text
+    fileNameP = T.pack <$> some (alphaNumChar <|> single '.' <|> single '/')
 
 lineOf ∷ Parser a → Parser a
 lineOf p = p <* newline
 
-data FileTree2 = FTDir Text [FileTree2]
+data FileTree = FTDir Text [FileTree]
                | FTFile Text Int
   deriving Show
 
--- now parse the stream of commandAndOutputP into a FileTree2
+-- now parse the stream of CommandAndOutput into a FileTree
 
 type ShellParser = Parsec Void Session
 
-fileTreeP ∷ ShellParser FileTree2
+-- These instances don't do anything useful, but they're here to convince 
+-- megaparsec to parse Sessions
+instance VisualStream Session where
+  showTokens :: Proxy Session → NonEmpty (Token Session) → String
+  showTokens _ = List.unwords . NE.toList . fmap show
+
+instance TraversableStream Session where
+  reachOffset :: Int → PosState s → (Maybe String, PosState s)
+  reachOffset _ s = (Nothing, s)
+
+fileTreeP ∷ ShellParser FileTree
 fileTreeP = dirP
 
-dirP ∷ ShellParser FileTree2
+dirP ∷ ShellParser FileTree
 dirP = do
   Cd dirname <- satisfy isCd
   Ls entries <- satisfy isLs
   childDirs <- many dirP
   try (void $ satisfy isCdUp) <|> eof
 
-  let children ∷ [FileTree2]
+  let children ∷ [FileTree]
       children = mapMaybe (\case (D _)         -> Nothing
                                  (F name size) -> Just $ FTFile name size)
                           entries
@@ -108,68 +106,34 @@ dirP = do
     isCdUp _    = False
 
 
-
 ---------------
 -- Solutions --
 ---------------
 
-nodes ∷ FileTree2 → [FileTree2]
+nodes ∷ FileTree → [FileTree]
 nodes t@(FTDir _ ts) = t : concatMap nodes ts
 nodes t@(FTFile _ _) = [t]
 
-directories ∷ FileTree2 → [FileTree2]
+directories ∷ FileTree → [FileTree]
 directories = filter isDir . nodes where
   isDir (FTDir _ _) = True
   isDir _           = False
 
-ftSize :: FileTree2 -> Int
+ftSize ∷ FileTree → Int
 ftSize (FTDir _ contents) = sum (map ftSize contents)
-ftSize (FTFile _ size) = size
+ftSize (FTFile _ size)    = size
 
-part1 ∷ FileTree2 → Int
+part1 ∷ FileTree → Int
 part1 = sum . filter (<=100000) . map ftSize . directories
 
-part2 ∷ FileTree2 → Int
-part2 fileTree = dirSizeToDelete
+part2 ∷ FileTree → Int
+part2 fileTree = minimum . filter (>=spaceToFree) $ sizes
   where
     totalUsedSpace = ftSize fileTree
     sizes = map ftSize . directories $ fileTree
     targetUsedSpace = 70000000 - 30000000
     spaceToFree = totalUsedSpace - targetUsedSpace
-    dirSizeToDelete = minimum . filter (>=spaceToFree) $ sizes
 
 main ∷ IO ()
-main = do
-  -- let input = parseInput exampleInput
-  -- print $ part1 input
-  -- other testing here
-  -- aocSinglePartMain "inputs/07.txt" exampleInput parseInput part1
-
+main =
   aocMain "inputs/07.txt" Solution { parse=parseInput, part1=part1, part2=part2 }
-
-exampleInput ∷ Text
-exampleInput = toText @String [str|$ cd /
-$ ls
-dir a
-14848514 b.txt
-8504156 c.dat
-dir d
-$ cd a
-$ ls
-dir e
-29116 f
-2557 g
-62596 h.lst
-$ cd e
-$ ls
-584 i
-$ cd ..
-$ cd ..
-$ cd d
-$ ls
-4060174 j
-8033020 d.log
-5626152 d.ext
-7214296 k
-|]
-
