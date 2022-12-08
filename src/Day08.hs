8de9adae3b5b52e9d047eb7b7769d3c0cbfae670
James Sully <sullyj3@gmail.com>
2022-12-08 17:58:48 +1100

    More day 08 cleanup

--- src/Day08.hs
+++ src/Day08.hs
@@ -4,17 +4,13 @@ module Day08 (main) where
 import           AOC
 import           AOC.Parse
 import           AOC.Parsers
-import qualified Data.Text   as T
-import           PyF
-import           Utils       (tRead, count)
-import Prelude hiding (some)
-import Text.Megaparsec.Char (digitChar)
-import Data.Char (digitToInt)
-import qualified Data.List as List
-import Data.Tree (unfoldForest)
-import Data.Vector (Vector, (!))
-import qualified Data.Vector as Vec
-import Data.List (maximum)
+import           Data.Char            (digitToInt)
+import           Data.List            (maximum)
+import           Data.Vector          (Vector, (!))
+import qualified Data.Vector          as Vec
+import           Prelude              hiding (some)
+import           Text.Megaparsec.Char (digitChar)
+import           Utils                (count)
 
 type Forest = [[Int]]
 
@@ -24,38 +20,34 @@ type Forest = [[Int]]
 -------------
 parseInput ∷ Text → Forest
 parseInput = unsafeParse (linesOf (some $ digitToInt <$> digitChar))
--- parseInput = toString
 
 ---------------
 -- Solutions --
 ---------------
 
-isVisibleFromLeft ∷ [Int] -> [Bool]
+isVisibleFromLeft ∷ [Int] → [Bool]
 isVisibleFromLeft = go minBound
   where
-    go currMax [] = []
+    go _ [] = []
     go currMax (n:ns)
       | currMax >= n = False : go currMax ns
       | otherwise = True : go n ns
 
-isVisibleFromEitherSide ∷ [Int] -> [Bool]
-isVisibleFromEitherSide ns = zipWith (||) 
+isVisibleFromEitherSide ∷ [Int] → [Bool]
+isVisibleFromEitherSide ns = zipWith (||)
   (isVisibleFromLeft ns) (reverse . isVisibleFromLeft . reverse $ ns)
 
 
-allVisible :: Forest -> [[Bool]]
+allVisible ∷ Forest → [[Bool]]
 allVisible forest = zip2dWith (||) hVisibles vVisibles
   where
     hVisibles = map isVisibleFromEitherSide forest
     vVisibles = transpose . map isVisibleFromEitherSide . transpose $ forest
 
-    zip2dWith :: (a -> b -> c) -> [[a]] -> [[b]] -> [[c]]
+    zip2dWith ∷ (a → b → c) → [[a]] → [[b]] → [[c]]
     zip2dWith f = zipWith (zipWith f)
 
-prettyVisible :: [[Bool]] -> Text
-prettyVisible = T.unlines . map (T.pack . map (\b -> if b then '#' else '.'))
-
-viewingDistance ∷ Int -> [Int] -> Int
+viewingDistance ∷ Int → [Int] → Int
 viewingDistance currHeight = go
   where
     go [] = 0
@@ -67,16 +59,16 @@ viewingDistance currHeight = go
 --
 -- Part 1
 --
-part1 :: Forest -> Int
+part1 ∷ Forest → Int
 part1 = sum . map (count id) . allVisible
 
 --
 -- Part 2
 --
-score :: (Int, Int) -> Vector (Vector Int) -> Int
-score (i, j) forest = 
+score ∷ (Int, Int) → Vector (Vector Int) → Int
+score (i, j) forest =
   product . map (viewingDistance heightHere . map getTree) $ [up, down, left, right]
-  where 
+  where
     heightHere = getTree (i, j)
     getTree (i', j') = forest ! i' ! j'
 
@@ -88,23 +80,15 @@ score (i, j) forest =
     height = Vec.length forest
     width = Vec.length (forest ! 0)
 
-part2 :: Forest -> Int
+part2 ∷ Forest → Int
 part2 (list2vec2d -> forest) = maximum do
   i <- [0..Vec.length forest - 1]
   j <- [0..Vec.length forest - 1]
   pure $ score (i, j) forest
-  
-list2vec2d :: [[a]] -> Vector (Vector a)
+
+list2vec2d ∷ [[a]] → Vector (Vector a)
 list2vec2d = Vec.fromList . map Vec.fromList
 
 main ∷ IO ()
 main = do
   aocMain "inputs/08.txt" Solution { parse=parseInput, part1=part1, part2=part2 }
-
-exampleInput :: Text
-exampleInput = toText @String [str|30373
-25512
-65332
-33549
-35390|]
-
