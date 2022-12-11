1df7ca92d6651753f1df5e98da1ed449a0858bf0
James Sully <sullyj3@gmail.com>
2022-12-11 15:59:05 +1100

    Update template

--- src/Day24.hs
+++ src/Day24.hs
@@ -6,42 +6,45 @@ import           AOC.Parse
 import           AOC.Parsers
 import PyF ( str )
 
--- ╔═╗┌─┐┬─┐┌─┐┬┌┐┌┌─┐                  
--- ╠═╝├─┤├┬┘└─┐│││││ ┬                  
--- ╩  ┴ ┴┴└─└─┘┴┘└┘└─┘                  
 
+-- |~) _  _ _. _  _                 
+-- |~ (_|| _\|| |(_|                
+--                _|                
 -- >>> parseInput $ exampleInput
-parseInput :: a -> a
-parseInput = id
--- parseInput = unsafeParse numLine
--- parseInput = toString
+parseInput :: Text -> [()]
+parseInput = unsafeParse $ linesOf $ pure ()
 
--- ╔═╗┌─┐┬─┐┌┬┐  ╔═╗┌┐┌┌─┐              
--- ╠═╝├─┤├┬┘ │   ║ ║│││├┤               
--- ╩  ┴ ┴┴└─ ┴   ╚═╝┘└┘└─┘              
+
+-- |~) _  __|_  /~\ _  _            
+-- |~ (_||  |   \_/| |(/_           
+--
 -- >>> part1 . parseInput $ exampleInput
 part1 :: a -> a
 part1 = id
 
--- ╔═╗┌─┐┬─┐┌┬┐  ╔╦╗┬ ┬┌─┐              
--- ╠═╝├─┤├┬┘ │    ║ ││││ │              
--- ╩  ┴ ┴┴└─ ┴    ╩ └┴┘└─┘              
 
+-- |~) _  __|_  ~|~  _              
+-- |~ (_||  |    |VV(_)             
+--
 -- >>> part2 . parseInput $ exampleInput
 part2 :: a -> a
 part2 = id
 
+
+-- |\/| _ . _                       
+-- |  |(_||| |                      
+--
 main ∷ IO ()
 main = do
   -- other testing here
 
   aocSinglePartMain "inputs/24.txt" exampleInput parseInput part1
-
   -- aocMain "inputs/24.txt" Solution { parse=parseInput, part1=part1, part2=part2 }
 
--- ╔═╗─┐ ┬┌─┐┌┬┐┌─┐┬  ┌─┐  ┬┌┐┌┌─┐┬ ┬┌┬┐
--- ║╣ ┌┴┬┘├─┤│││├─┘│  ├┤   ││││├─┘│ │ │ 
--- ╚═╝┴ └─┴ ┴┴ ┴┴  ┴─┘└─┘  ┴┘└┘┴  └─┘ ┴
+
+-- (~   _  _ _  _ | _   . _  _   _|_
+-- (_><(_|| | ||_)|(/_  || ||_)|_|| 
+--             |            |
 exampleInput :: Text
 exampleInput = toText @String [str||]
 
